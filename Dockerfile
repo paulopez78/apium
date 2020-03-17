b47c27743144fff238c6456ad2d79edf1b92b04c
paulopez78 <pau.lopez.78@gmail.com>
2020-03-17 23:26:18 +0100

    sesion3

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM ubuntu
-
-RUN apt-get update && apt-get install curl -y && rm -rf /var/lib/apt/lists/*
-
-ENTRYPOINT [ "curl" ]
--- pipeline.sh
+++ pipeline.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
-cd src/votingapp
-pkill votingapp || echo "Cleaning up..." && true
-echo $?
-go build -o votingapp
-echo $?
-./votingapp 1> log.txt &
-echo $?
-curl http://localhost:5000/vote
 
-    echo "Deploying to prod..."
-# if [[ $? -gt 0 ]] ; then
-#     echo "Failed!"
-# else
-#     echo "Deploying to prod..."
-# fi
+#unit test build
+# pushd ./src/votingapp
+# go test
+# GOOS=linux go build -o build/votingapp
+# cp -r ./ui ./build
+# popd
+
+docker build -t paulopez/votingapp:0.2 ./src/votingapp
+
+# integration test
+docker rm -f voting || true
+docker run -p 8080:5000 --name voting -d paulopez/votingapp:0.2
+curl http://localhost:8080/vote
+
+ #delivery
+docker push paulopez/votingapp:0.2
+echo "Deploying to prod..."
